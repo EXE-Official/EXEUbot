b529fc3ea98b7384dd3f9b3cea3ccee7fdb107a0
Elxes04 <elol03468@gmail.com>
2024-06-23 11:06:49 +0200

    Fix missing translations for leave.py

--- commands/leave.py
+++ commands/leave.py
@@ -1,18 +1,19 @@
 from telethon import events
+from translations import translations
 
 async def leave_group(event, client):
     try:
         if event.is_group:
             chat_id = event.chat_id
             
-            await event.respond("I left the group.")
+            await event.respond(translations['left_group'])
             
             
             await client.delete_dialog(chat_id)
         else:
-            await event.respond("This command can only be used in groups.")
+            await event.respond(translations['command_groups_only'])
     except Exception as e:
-        await event.respond(f"An error occurred: {str(e)}")
+        await event.respond(translations['error_occurred'].format(error=str(e)))
 
 def register(client):
     @client.on(events.NewMessage(pattern=r'^\.leave$', outgoing=True))
