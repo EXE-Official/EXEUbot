c564aaf8b8d4e79f91ae68b9cb9995523d9a23c0
Elxes04 <elol03468@gmail.com>
2024-07-04 12:06:26 +0200

    Added support for translations

--- commands/info.py
+++ commands/info.py
@@ -3,6 +3,7 @@ import speedtest
 import platform
 import humanize
 from telethon import TelegramClient, events
+from translations import translations
 
 async def system_info(event):
     try:
@@ -18,23 +19,23 @@ async def system_info(event):
         telethon_version = TelegramClient.__version__
 
         message = (
-            f"ℹ️ System info:\n"
-            f"💻 OS: {system_info.system}\n"
-            f"🔳 Kernel: {system_info.release}\n"
-            f"🐍 Python version: {python_version}\n"
-            f"🤖 Telethon version: {telethon_version}\n"
-            f"⚙️ Userbot version: Beta 1.14\n"
-            f"⚡ CPU usage: {cpu_usage}%\n"
-            f"📈 RAM usage: {memory.percent}% ({humanize.naturalsize(memory.used)})\n"
-            f"📉 RAM total: {humanize.naturalsize(memory.total)}\n"
-            f"📶 Ping: {ping} ms\n"
-            f"🤖 [Comandi Userbot](https://telegra.ph/Userbot-commands-09-11-46)\n"
-            f"👤 Userbot created by Elxes ©️"
+            f"{translations.get('system_info_title')}\n"
+            f"{translations.get('os')}: {system_info.system}\n"
+            f"{translations.get('kernel')}: {system_info.release}\n"
+            f"{translations.get('python_version')}: {python_version}\n"
+            f"{translations.get('telethon_version')}: {telethon_version}\n"
+            f"{translations.get('userbot_version')}: Beta 1.15\n"
+            f"{translations.get('cpu_usage')}: {cpu_usage}%\n"
+            f"{translations.get('ram_usage')}: {memory.percent}% ({humanize.naturalsize(memory.used)})\n"
+            f"{translations.get('ram_total')}: {humanize.naturalsize(memory.total)}\n"
+            f"{translations.get('ping')}: {ping} ms\n"
+            f"{translations.get('userbot_commands_link')}\n"
+            f"{translations.get('userbot_created_by')}"
         )
 
         await event.edit(message)
     except Exception as e:
-        await event.reply(f"An error occurred while reading system information: {str(e)}")
+        await event.reply(translations.get('error_occurred', error=str(e)))
         print(str(e))
 
 def register(client):
