d1967844b1992e439a476aa3382bbdf9e08c6937
NoEscape-EXE <NoEscape@disroot.org>
2024-07-05 15:48:39 +0200

    Added .delall command and updated translation

---
+++ commands/delall.py
@@ -0,0 +1,35 @@
+import asyncio
+from telethon import events
+from telethon.errors import FloodWaitError
+from translations import translations
+
+async def delete_all_messages(event, client):
+    chat_id = event.chat_id
+    message_ids = []
+    
+    async for msg in client.iter_messages(chat_id):
+        message_ids.append(msg.id)
+        
+        if len(message_ids) >= 100:
+            try:
+                await client.delete_messages(chat_id, message_ids)
+                message_ids = []
+                await asyncio.sleep(1)  # Aggiungi un ritardo di 1 secondo tra le eliminazioni
+            except FloodWaitError as e:
+                wait_time = e.seconds
+                print(translations['floodwait_error'].format(waitingtime=str(wait_time)))
+                await asyncio.sleep(wait_time)
+            except Exception as e:
+                print(translations['error_occurred'].format(error=str(e)))
+    
+    # Elimina eventuali messaggi rimanenti
+    if message_ids:
+        try:
+            await client.delete_messages(chat_id, message_ids)
+        except Exception as e:
+            print(translations['error_occurred'].format(error=str(e)))
+
+def register(client):
+    @client.on(events.NewMessage(pattern=r'^\.delall$', outgoing=True))
+    async def handler(event):
+        await delete_all_messages(event, client)
